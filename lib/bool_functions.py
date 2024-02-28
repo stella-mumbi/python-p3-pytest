f20d6e0107bdfc17fc0e7a6fe346809e15473fa1
stella-mumbi <stella.mumbi@student.moringaschool.com>
2024-02-28 21:48:22 +0300

    completed assignmnet

--- lib/bool_functions.py
+++ lib/bool_functions.py
@@ -1,2 +1,4 @@
+# bool_functions.py
+
 def return_true():
-    return False
+    return True
--- lib/testing/subdirectory/bool_test.py
+++ lib/testing/subdirectory/bool_test.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+# bool_test.py
 
 from bool_functions import return_true
 
 def test_return_true():
     '''in bool_functions, function "return_true" returns True.'''
-    assert return_true() == True
+    assert return_true()  # You don't need to compare to True explicitly
